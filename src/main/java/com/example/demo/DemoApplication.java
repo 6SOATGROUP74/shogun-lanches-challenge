3f296f3e2ad9ee74ed3971d90453a0ec370a89f7
igusvs <igoralves159@gmail.com>
2024-06-30 12:00:44 -0300

    configurar env

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,21 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.example.demo.adapter.outbound.repository")
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Environment Variables:");
+		System.getenv().forEach((key, value) -> System.out.println(key + "=" + value));
+	}
 }
