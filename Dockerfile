7a89ab21f69eeb4f69b2cb5d1706941f2a00deaa
igusvs <igoralves159@gmail.com>
2024-07-21 20:19:05 -0300

    ajuste infra k8s / aws

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ MAINTAINER 2024_6SOAT
 
 COPY target/tech-challenge-1.jar app.jar
 
+ENV SPRING_PROFILES_ACTIVE=prod
+
 ENTRYPOINT ["java", "-jar", "/app.jar"]
 
 EXPOSE 8080
--- infra/stress.sh
+++ infra/stress.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in {1..10000}; do
-  curl localhost:30000
+  curl a3185ec998f53407a8abe8a9514cf407-2073599187.us-east-1.elb.amazonaws.com
   sleep $1
 done
