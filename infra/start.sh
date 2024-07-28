fbdd95a3e60fdf42c7f773bd0506f4262f13bfbd
igusvs <igoralves159@gmail.com>
2024-07-28 12:25:43 -0300

    Ajuste cd

--- infra/start.sh
+++ infra/start.sh
@@ -10,12 +10,8 @@ sleep 1
 
 kubectl apply -f ./service-account.yaml
 kubectl apply -f ./app/app-nlb.yaml
+kubectl apply -f ./app/app-svc.yaml
 kubectl apply -f ./app/app-configmap.yaml
 kubectl apply -f ./app/app-deployment.yaml
 kubectl apply -f ./app/app-hpa.yaml
 kubectl apply -f ./app/app-opaque.yaml
-kubectl apply -f ./app/app-svc.yaml
-
-
-
-
