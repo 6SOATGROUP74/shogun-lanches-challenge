3934bc4ae9facbb37d919ffc5fd72ae4ed32a9c8
igusvs <igoralves159@gmail.com>
2024-07-28 18:17:14 -0300

    Inclusao de deployment mysql

--- infra/start.sh
+++ infra/start.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-kubectl apply -f ./database/mysql-configmap.yaml
 kubectl apply -f ./database/mysql-opaque.yaml
-kubectl apply -f ./database/mysql-pod.yaml
+kubectl apply -f ./database/mysql-deployment.yaml
 kubectl apply -f ./database/mysql-pvc.yaml
 kubectl apply -f ./database/mysql-svc.yaml
 
-sleep 1
+sleep 3
 
 kubectl apply -f ./service-account.yaml
 kubectl apply -f ./app/app-nlb.yaml
