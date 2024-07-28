5b17dab69bca9b99af2aa6485c41fc12bd4d8afc
igusvs <igoralves159@gmail.com>
2024-07-28 08:02:28 -0300

    remove logs de env

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,16 +11,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.example.demo.adapter.outbound.repository")
 @EnableFeignClients
-public class DemoApplication implements CommandLineRunner {
+public class DemoApplication {
 
 	private static final Logger logger = LogManager.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-	@Override
-	public void run(String... args) throws Exception {
-		System.getenv().forEach((key, value) -> logger.info("{}={}", key, value));
 	}
 
 }
