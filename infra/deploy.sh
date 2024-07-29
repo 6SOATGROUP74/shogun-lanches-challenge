073d771f431ed9712aebfa334a492a3fbbd89790
igusvs <igoralves159@gmail.com>
2024-07-29 20:44:37 -0300

    renomear nome arquivo

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-
 kubectl apply -f ./database/mysql-opaque.yaml
 kubectl apply -f ./database/mysql-deployment.yaml
 kubectl apply -f ./database/mysql-pvc.yaml
 kubectl apply -f ./database/mysql-svc.yaml
-
 sleep 3
 kubectl apply -f ./service-account.yaml
-
 sleep 3
-
 kubectl apply -f ./app/app-nlb.yaml
 kubectl apply -f ./app/app-svc.yaml
 kubectl apply -f ./app/app-configmap.yaml
